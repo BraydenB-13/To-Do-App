418946d6b175a5969386c8b24ce8a3c7393dcbca
BraydenB-13 <brayden.barger8273@stu.mtec.edu>
2020-10-17 22:54:14 -0600

    edit

--- js/more.js
+++ js/more.js
@@ -1,2 +1,5 @@
 const editTaskButton = document.querySelector('[data-edit-task-button]')
 const deleteTaskButton = document.querySelector('[data-delete-task-button]')
+
+editTaskButton.addEventListener('click', e => {
+})
